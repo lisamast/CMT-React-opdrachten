31028874424ea9c9d405062ba3872e594b008267
lisamast <9027431@student.zadkine.nl>
2025-10-17 10:41:33 +0200

    react opdracht 2

---
+++ opdracht-02-useEffect/src/components/Clock.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+
+const Clock = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(new Date())
+        }, 1000)
+
+            return () => {
+        clearInterval(intervalId);
+        };
+    }, []);
+
+    return ( 
+        <h2>{currentTime.toLocaleTimeString()}</h2>
+     );
+}
+
+export default Clock;
+
